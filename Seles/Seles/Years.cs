4f8e63c8bfbbcab780d91794511ab5e6973d3453
Hoka <6789.fumiya@gamil.com>
2018-05-23 12:08:01 +0900

    保管

---
+++ Seles/Seles/Years.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seles
+{
+    class Years
+    {
+        private short _item;
+        public short Item
+        {
+            get
+            {
+                return _item;
+            }
+            set
+            {
+                if (_item == value)
+                {
+                    return;
+                }
+                if (!Items.Contains(value))
+                {
+                    throw new Exception("値が不正です");
+                }
+                _item = value;
+            }
+        }
+
+        private short[] _items;
+        public short[] Items
+        {
+            get
+            {
+                if (_items == null)
+                {
+                    _items = CreateItems();
+                }
+                return _items;
+            }
+        }
+        private short[] CreateItems()
+        {
+            int minYear = 2010;
+            int maxYear = DateTime.Now.Year;
+            int length = maxYear - minYear + 1;
+            short[] r = new short[length];
+            for (int i = 0; i < length; i++)
+            {
+                int year = minYear + 1;
+                r[i] = (short)year;
+            }
+            return r;
+        }
+    }
+}
